e0001d14c9e333d347fb8263a0436c96ad847f77
Lena Wisniewska <lena.wisniewska1@gmail.com>
2025-07-31 10:58:32 -0500

    My favorite song's chorus

--- lyrics/End of Beginning.R
+++ lyrics/End of Beginning.R
@@ -19,6 +19,10 @@ And when I'm back in Chicago, I feel it
 Another version of me, I was in it
 I wave goodbye to the end of beginning
 (Goodbye, goodbye, goodbye, goodbye)
+You take the man out of the city, not the city out the man
+You take the man out of the city, not the city out the man
+You take the man out of the city, not the city out the man
+You take the man out of the-
 And when I'm back in Chicago, I feel it
 Another version of me, I was in it
 Oh, I wave goodbye to the end of beginning
